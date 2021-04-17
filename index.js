dc4b7147207947435be38682cd2874971d0e5654
Ben-number-4 <76911556+Ben-number-4@users.noreply.github.com>
2021-04-17 08:10:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function getStylesheets() {
         var styling = document.createElement("style");
         styling.innerHTML = "@import url('" + dish + "')";
         document.body.appendChild(styling);
-        styling.parentNode.removeChild(styling);
   }
 }
 
@@ -18,7 +17,6 @@ function getScripts() {
         var scription = document.createElement("style");
         scription.src = dish;
         document.body.appendChild(scription);
-        styling.parentNode.removeChild(scription);
   }
 }
 
