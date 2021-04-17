79b0f217df3c5cd38b5d0706001da69ed4925557
Ben-number-4 <76911556+Ben-number-4@users.noreply.github.com>
2021-04-17 08:15:45 -0700

    Add files via upload

---
+++ download.js
@@ -0,0 +1,9 @@
+function download(url, name) {
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = url;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    delete link;
+}
