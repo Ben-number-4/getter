e596000025af6aa659dd92b0cae200be24021934
Ben-number-4 <76911556+Ben-number-4@users.noreply.github.com>
2021-04-24 08:23:45 -0700

    Update get text.js

--- get text.js
+++ get text.js
@@ -1,10 +1,12 @@
-const fs = require('fs');
-
-function getText() {
-    var texts = new Array();
+const getText = () => {
     for(var i in arguments) {
-        var read = fs.readFile(arguments[i-1]);
-        texts.push(read);
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // Typical action to be performed when the document is ready:
+                document.getElementById("demo").innerHTML = xhttp.responseText;
+            }
+        };
+        xhttp.open("GET", "filename", true);
     }
-    return texts;
 }
